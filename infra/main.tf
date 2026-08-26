53c3fb1253d8ba5f028185c174ac1a033ac54b2e
Abhimanyu Sharma <abhimanyu@abhimanyus-mac.local>
2026-08-27 01:52:56 +0530

    feat: add ~30 deliberate misconfigurations to Terraform seed

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,6 @@ provider "aws" {
   region                      = var.region
   access_key                  = var.access_key
   secret_key                  = var.secret_key
-  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
